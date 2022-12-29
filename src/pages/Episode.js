b70adec4ec0b7025490e7feab99f056c879649d2
anangyoga <aharwinto@gmail.com>
2022-12-29 23:26:04 +0700

    feat: fetch episode

--- src/pages/Episode.js
+++ src/pages/Episode.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MainLayout from "../layout/MainLayout";
+import api from "../config/api";
 
 const Episode = () => {
-  return <div>Episode</div>;
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    api.get("episode").then((res) => setEpisodes(res.data));
+  }, []);
+
+  console.log(episodes);
+  return (
+    <MainLayout>
+      {episodes.results.map((item) => (
+        <p>{item.name}</p>
+      ))}
+    </MainLayout>
+  );
 };
 
 export default Episode;
