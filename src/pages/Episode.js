6995d92016e065b5096699ea47dfbf12f2e0db4b
anangyoga <aharwinto@gmail.com>
2022-12-31 13:48:53 +0700

    fix: fetch episode

--- src/pages/Episode.js
+++ src/pages/Episode.js
@@ -6,16 +6,28 @@ const Episode = () => {
   const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
-    api.get("episode").then((res) => setEpisodes(res.data));
+    api.get("episode").then((res) => setEpisodes(res.data?.results));
   }, []);
 
-  console.log(episodes);
+  console.log(episodes, "yuk");
   return (
-    <MainLayout>
-      {episodes.results.map((item) => (
-        <p>{item.name}</p>
-      ))}
-    </MainLayout>
+    <>
+      <h1
+        className="text-3xl font-bold  text-emerald-500 text-center
+      mt-2 "
+      >
+        Episodes
+      </h1>
+
+      <MainLayout>
+        {episodes.map((item) => (
+          <div className="border rounded-lg max-w-md shadow-md p-4">
+            <p>Episode: {item.episode}</p>
+            <p>Title: {item.name}</p>
+          </div>
+        ))}
+      </MainLayout>
+    </>
   );
 };
 
